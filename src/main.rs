51bde406a4197302b5a1cdd6b038d4435aee5adb
Jooyon Won <jyon1204@gmail.com>
2025-05-10 00:27:39 -0700

    first commit

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+/// This is comment for the function
+fn main() {
+    scope();
+    data_types();
+}
+
+fn scope() {
+    let x = 5;
+    let x = x + 1;
+    print!("{}\n", x);
+    {
+        let x = x * 2;
+        print!("{}\n", x);
+        {
+            print!("{}\n", x);
+            // x + 100;
+            print!("{}\n", x);
+            let x = x * 200;
+            {
+                print!("{}\n", x);
+            }
+        }
+    }
+    print!("{}\n", x);
+
+    let sn: u32 = "42".parse().expect("NaN");
+    print!("{}\n", sn);
+
+    let arbitrary_num: u32 = 1_000_000;
+    print!("{}\n", arbitrary_num);
+}
+
+fn data_types() {
+    let sum = 5 + 10;
+    println!("sum: {}", sum);
+    let diff = 95.5 - 4.3;
+    println!("diff: {}", diff);
+    let prod = 4*30;
+    println!("prod: {}", prod);
+    let quotient = 56.7/32.2;
+    println!("quotient: {}", quotient);
+    let trunc = -5/3;
+    println!("trunc: {}", trunc);
+    let remainder = 43%5;
+    println!("remainder: {}", remainder);
+}
